140d8fc7cae98c1df33db5f07b7656d57f41cbe7
Lucy2181 <97625777+Lucy2181@users.noreply.github.com>
2023-01-24 14:25:04 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # # . # #
+            # # # # #
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
